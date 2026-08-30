7830ff9b6414049624cff0fa5bb6a8d70982e9da
mumn <mumn06n@gmail.com>
2026-08-30 10:28:42 +0900

    DO3号追加

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const API_URLS = {
     water: "/api/water",
     salinity: "/api/salinity",
     do1: "/api/do1",
+    do3: "/api/do3", //追加
 };
 // Vite の環境変数から API URL を取得
 
@@ -53,6 +54,10 @@ const renderTable = (data: string, sensorType: string): string => {
 
     if (sensorType === "do1"){
         return renderDO1Table(data);
+    }
+    //追加
+    if (sensorType === "do3"){
+        return renderDO3Table(data);
     }
 
     return "";
@@ -195,8 +200,56 @@ function renderDO1Table(data: string): string {
 
     return tableHtml;
 }
+//追加
+function renderDO3Table(data: string): string {
+    const rows = parseData(data);
+
+    let tableHtml = `
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>センサID</th>
+                    <th>日時</th>
+                    <th>バッテリ電圧</th>
+                    <th>外気温</th>
+                    <th>水温</th>
+                    <th>DO(%)</th>
+                    <th>DO(mg/L)</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+    rows.reverse().forEach((row: any) => {
+
+        const utcDate = new Date(row[1]);
+        const jst = utcDate.toLocaleString("ja-JP", {
+            timeZone: "Asia/Tokyo"
+        });
+
+        tableHtml += `
+            <tr>
+                <td>${row[0]}</td>
+                <td>${jst}</td>
+                <td>${row[2]} V</td>
+                <td>${row[3]} ℃</td>
+                <td>${row[4]} ℃</td>
+                <td>${row[5]} %</td>
+                <td>${row[6]} mg/L</td>
+            </tr>
+        `;
+    });
+
+    tableHtml += `
+            </tbody>
+        </table>
+    `;
+
+    return tableHtml;
+}
 
 if (app) {
+    //button追加
     app.innerHTML = `
     <h2>センサデータ</h2>
 
@@ -204,6 +257,8 @@ if (app) {
         <button id="water-tab" class="tab active">水温</button>
         <button id="salinity-tab" class="tab">塩分</button>
         <button id="do1-tab" class="tab">DO1号</button>
+        <button id="do3-tab" class="tab">DO3号</button>
+
     </div>
 
     <div id="table-container">
@@ -214,6 +269,7 @@ if (app) {
 const waterTab = document.getElementById("water-tab");
 const salinityTab = document.getElementById("salinity-tab");
 const do1Tab = document.getElementById("do1-tab");
+const do3Tab = document.getElementById("do3-tab");//追加
 
 waterTab?.addEventListener("click", () => {
     setActiveTab(waterTab);
@@ -227,6 +283,11 @@ salinityTab?.addEventListener("click", () => {
 do1Tab?.addEventListener("click", () => {
     setActiveTab(do1Tab);
     loadTable(API_URLS.do1, "do1");
+});
+
+do3Tab?.addEventListener("click", () => {
+    setActiveTab(do3Tab);
+    loadTable(API_URLS.do3, "do3");
 });
 
 // API からデータを取得して表を反映

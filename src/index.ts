61528c1bfc60edccd8b78cb868ef590d5e5b5d2d
mumn <mumn06n@gmail.com>
2026-08-30 20:27:48 +0900

    横スクロール修正

--- src/index.ts
+++ src/index.ts
@@ -463,7 +463,7 @@ function renderWaterChart(data: string) {
     ) as HTMLCanvasElement;
 
     const chartWidth = Math.max(
-        rows.length * 50,
+        200,
         800
     );
 
@@ -534,7 +534,7 @@ function renderSalinityChart(data: string) {
     ) as HTMLCanvasElement;
 
     const chartWidth = Math.max(
-        rows.length * 50,
+        200,
         800
     );
 
@@ -614,7 +614,7 @@ function renderDO1Chart(data: string) {
     ) as HTMLCanvasElement;
 
     const chartWidth = Math.max(
-        rows.length * 50,
+        200,
         800
     );
 
@@ -699,7 +699,7 @@ function renderDO3Chart(data: string) {
     ) as HTMLCanvasElement;
 
     const chartWidth = Math.max(
-        rows.length * 50,
+        200,
         800
     );
 
